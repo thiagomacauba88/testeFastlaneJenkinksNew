6efa16e9c69517acd3a15328544631f9ba69814c
Thiago Freitas <thiagohj>
2019-02-18 17:42:29 -0300

    firs

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source "https://rubygems.org"
+
+gem "fastlane"
+
+# Added at 2019-01-25 15:36:47 -0200 by thiago:
+gem "cocoapods", "~> 1.5"
---
+++ Pods/Target Support Files/Pods-TesteFastlaneJenkinsUITests/Pods-TesteFastlaneJenkinsUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TesteFastlaneJenkinsUITests : NSObject
+@end
+@implementation PodsDummy_Pods_TesteFastlaneJenkinsUITests
+@end
